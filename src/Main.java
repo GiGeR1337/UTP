5aef67b1c50dc5b8d47f1013916dbf9a3d374dbf
s30176 <s30176@pjwstk.edu.pl>
2024-10-07 10:43:46 +0200

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 // TODO: we need to add the missing classes!
 
-// OK, I will add `Adder` and s##### will add `Subtractor`.
+// OK, I will add `Adder` and s30176 will add `Subtractor`.
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
